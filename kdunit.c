0437104d8516ab84c5d09a747003100a80f7e8f6
Daniel Beall <daniel@dantop.ncsu.edu>
2017-10-06 15:45:31 -0400

    Implementing basic API #2

---
+++ kdunit.c
@@ -0,0 +1,45 @@
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+#include "kdunit.h"
+
+
+void assertNull(const void *p)
+{
+  assert(p == NULL);
+}
+
+void assertNotNull(const void *p)
+{
+  assert(p != NULL);
+}
+
+void assertStringEquals(const char *expected, const char *actual)
+{
+  assert(strcmp(expected, actual)==0);
+}
+
+void assertDoubleEquals(const double expected, const double actual, const double delta)
+{
+  assert(fabs(expected-actual)<= delta);
+}
+
+void assertFloatEquals(const float expected, const float actual, const float delta)
+{
+  assert(fabs(expected-actual)<= delta);
+}
+
+void assertLongEquals(const long expected, const long actual)
+{
+  assert(expected == actual);
+}
+
+void assertIntEquals(const int expected, const int actual)
+{
+  assert(expected == actual);
+}
+
+void assertCharEquals(const char expected, const char actual)
+{
+  assert(expected == actual);
+}
